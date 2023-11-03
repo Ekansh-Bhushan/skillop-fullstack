b2c3640b7f9422a40000116d449b0aaef690f013
automatefreely <automatefreely@gmail.com>
2023-11-04 00:16:23 +0530

    fdgf

--- frontend/src/components/Profile/EditPic/EditPic.js
+++ frontend/src/components/Profile/EditPic/EditPic.js
@@ -10,7 +10,7 @@ import { getUser } from "../../../api/userRequest";
 import { useEffect } from "react";
 import { toast } from "react-hot-toast";
 
-const API = axios.create({ baseURL: "https://skillop-back.onrender.com" });
+const API = axios.create({ baseURL: "https://app.skillop.in" });
 
 function EditPic({ userData, setProgress }) {
     const [uploading, setUploading] = useState(false);
