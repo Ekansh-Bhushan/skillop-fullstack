98065a17f19336cb3bfef675a50be354a63949ee
Aman Tiwari <amananjalitiwari2007@gmail.com>
2023-11-14 10:27:23 +0530

    new

--- backend/src/controllers/mentor.js
+++ backend/src/controllers/mentor.js
@@ -1054,19 +1054,12 @@ exports.setDataForMentor = async (req, res) => {
     }
 };
 
-const GOOGLE_CLIENT_ID = "154719299730-irqnpdj9jo8n2pa475b0gbpmoi78orha.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET = "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"
-
-// const oauth2Client = new google.auth.OAuth2(
-//     process.env.GOOGLE_CLIENT_ID,
-//     process.env.GOOGLE_CLIENT_SECRET,
-//     ' http://localhost:3000'
-// );
 const oauth2Client = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    ' http://localhost:3000'
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    ' https://app.skillop.in'
 );
+
 const REFRESH_TOKEN = null;
 exports.createToken = async (req, res) => {
     try {
