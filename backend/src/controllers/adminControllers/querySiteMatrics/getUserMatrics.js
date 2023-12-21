3e4d47f095cf3e45fa7cae13f6aa28402a003108
automatefreely <automatefreely@gmail.com>
2023-12-21 23:16:28 +0530

    event

--- backend/src/controllers/adminControllers/querySiteMatrics/getUserMatrics.js
+++ backend/src/controllers/adminControllers/querySiteMatrics/getUserMatrics.js
@@ -2,6 +2,7 @@
 // logic:__lastVistird - now >30days
 
 const User = require("../../../models/user");
+const Comment = require("../../../models/comment");
 const {
     response_200,
     response_500,
@@ -46,11 +47,58 @@ exports.getMostFollowedUser = async (req, res) => {
         let users = await User.find({});
         users.sort((a, b) => b.followers.length - a.followers.length);
         users = users.slice(0, GET_TOP_OF);
-        return response_200(res, "Most followed users fetched successfully", users);
+        return response_200(
+            res,
+            "Most followed users fetched successfully",
+            users
+        );
     } catch (error) {
         response_500(res, "Error while fetching mentors", error);
     }
-}
+};
 
-// MOST ACTIVE USERS LIST ( POSTS COMMENTS LIKES)
+// MOST ACTIVE USERS LIST ( POSTS
+// COMMENTS)
 
+exports.getMostActiveUser = async (req, res) => {
+    try {
+        const GET_TOP_OF = req.query.getTopOf || 10;
+        const POST_COMMENT_RATIO = req.query.postCommentRatio || 1;
+        let users = await User.find({});
+
+        const peoples = {};
+        for (let user of users) {
+            if (user.posts) user.__totalPosts = user.posts.length;
+            else user.__totalPosts = 0;
+
+            const commentByUser = await Comment.find({ user: user._id });
+            user.__totalComments = commentByUser.length;
+        }
+        
+        users.sort(
+            (a, b) =>
+                b.__totalPosts * POST_COMMENT_RATIO +
+                b.__totalComments -
+                (a.__totalPosts * POST_COMMENT_RATIO + a.__totalComments)
+        );
+        users = users.slice(0, GET_TOP_OF).map((user) => {
+            return {
+                _id: user._id,
+                name: user.name,
+                username: user.username,
+                __totalPosts: user.__totalPosts,
+                __totalComments: user.__totalComments,
+                profilePicUrl: user.profilePicUrl,
+
+            }
+        });
+
+        return response_200(
+            res,
+            "Most active users fetched successfully",
+            users
+        );
+    } catch (error) {
+        response_500(res, "Error while fetching mentors", error);
+    }
+};
--- backend/src/routes/adminRoutes/adminActionRoutes.js
+++ backend/src/routes/adminRoutes/adminActionRoutes.js
@@ -1,7 +1,14 @@
+const { addAdminEvent, deleteAdminEvent, getAllAdminEvents } = require("../../controllers/adminControllers/adminActions/manageEvents");
 const { AdminSendNotification } = require("../../controllers/adminControllers/adminActions/manageNotification");
 
 const router = require("express").Router();
 
 router.route("/send/notification").post(AdminSendNotification);
+
+// Events
+router.route("/event/create").post(addAdminEvent);
+router.route("/event/delete/:eventId").delete(deleteAdminEvent);
+router.route("/event/all").get(getAllAdminEvents);
+
 
 module.exports = router;
