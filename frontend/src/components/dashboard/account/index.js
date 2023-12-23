e678ddad5ff437099bdc4ead9cd93a6ec152d3b6
vbirla789 <birlavishal90@gmail.com>
2023-12-23 16:52:16 +0530

    meet page added mobile

--- frontend/src/components/dashboard/account/index.js
+++ frontend/src/components/dashboard/account/index.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import linkedin from '../../images/linkedin.png';
-import google from '../../images/google.png';
-import Header1 from '../../Header';
-import './index.css';
-import { Navigate, useNavigate } from 'react-router-dom';
-import Commondash from '../common';
-import Topbar from '../topbar';
-import { updateIsMentor } from '../../../api/userRequest';
-import UPIicon from '../../images/UPI.png';
-import Profileandevents from '../../Landing/Profileandevents';
-import { toast } from 'react-hot-toast';
-import Mobilecommonhead from '../../Mobilecommonhead';
+import React, { useEffect, useState } from "react";
+import linkedin from "../../images/linkedin.png";
+import google from "../../images/google.png";
+import Header1 from "../../Header";
+import "./index.css";
+import { Navigate, useNavigate } from "react-router-dom";
+import Commondash from "../common";
+import Topbar from "../topbar";
+import { updateIsMentor } from "../../../api/userRequest";
+import UPIicon from "../../images/UPI.png";
+import Profileandevents from "../../Landing/Profileandevents";
+import { toast } from "react-hot-toast";
+import Mobilecommonhead from "../../Mobilecommonhead";
 
 function Account({ userData, setProgress, Mentor, isFetched, notifyList }) {
   /*---------LINKING ROUTES---------*/
@@ -26,24 +26,24 @@ function Account({ userData, setProgress, Mentor, isFetched, notifyList }) {
   useEffect(() => {
     // This function will run when the DOM content is loaded.
     console.log(email, whats, upi, linkedindata);
-    var inp1 = document.querySelectorAll('.editable-input');
+    var inp1 = document.querySelectorAll(".editable-input");
     for (var i = 0; i <= 3; i++) {
       inp1[i].disabled = true;
     }
   }, []);
 
   const enabledisableinput = async () => {
-    var inp = document.querySelectorAll('.editable-input');
+    var inp = document.querySelectorAll(".editable-input");
     if (inp[0].disabled) {
       for (var i = 0; i <= 3; i++) {
         inp[i].disabled = false;
       }
-      document.querySelector('.edit-input').style.backgroundColor = '#108CFF';
+      document.querySelector(".edit-input").style.backgroundColor = "#108CFF";
       // document.querySelector(".edit-input").style.color = "black";
-      document.querySelector('.edit-input').textContent = 'Save';
-      for (let item of document.querySelectorAll('.editable-input')) {
-        item.style.border = '2px solid black';
-        item.style.borderRadius = '10px';
+      document.querySelector(".edit-input").textContent = "Save";
+      for (let item of document.querySelectorAll(".editable-input")) {
+        item.style.border = "2px solid black";
+        item.style.borderRadius = "10px";
       }
     } else {
       try {
@@ -57,7 +57,7 @@ function Account({ userData, setProgress, Mentor, isFetched, notifyList }) {
         await updateIsMentor(JSON.stringify(request));
         // console.log(data);
         setProgress(100);
-        toast.success('Account info updated!');
+        toast.success("Account info updated!");
         // console.log(data);
       } catch (error) {
         setEmail(userData.email);
@@ -73,13 +73,13 @@ function Account({ userData, setProgress, Mentor, isFetched, notifyList }) {
         inp[i].disabled = true;
       }
 
-      for (let item of document.querySelectorAll('.editable-input')) {
-        item.style.border = 'none';
+      for (let item of document.querySelectorAll(".editable-input")) {
+        item.style.border = "none";
       }
 
       // document.querySelector(".edit-input").style.backgroundColor = "black";
-      document.querySelector('.edit-input').style.color = 'white';
-      document.querySelector('.edit-input').textContent = 'Edit';
+      document.querySelector(".edit-input").style.color = "white";
+      document.querySelector(".edit-input").textContent = "Edit";
     }
   };
 
@@ -93,12 +93,12 @@ function Account({ userData, setProgress, Mentor, isFetched, notifyList }) {
       /> */}
       <Mobilecommonhead />
 
-      <div className="dash-main md:px-5">
+      <div className="dash-main">
         {/* <Commondash userData={ userData} /> */}
 
         <div className="dash-right dash-right-2">
           {/* <Topbar setProgress={setProgress}/> */}
-          <div className="pt-[10vh] h-[100vh] border-r-2 md:border-0">
+          <div className="pt-[10vh] h-[100vh] border-r-2 md:border-0 md:pt-[6vh]">
             <h1 className="font-semibold text-2xl mb-5">
               Edit Account Information
             </h1>
@@ -168,7 +168,7 @@ function Account({ userData, setProgress, Mentor, isFetched, notifyList }) {
             <div className="editor-ids">
               <button
                 className="edit-pass"
-                onClick={() => navigate('/changePassword')}
+                onClick={() => navigate("/changePassword")}
               >
                 Change Password
               </button>
