8b880da0b20d7e3caf9c367a309165b1c8509341
automatefreely <automatefreely@gmail.com>
2023-12-29 17:39:48 +0530

    reset password

--- frontend/src/components/resetpassword/resetpasswordEmail.js
+++ frontend/src/components/resetpassword/resetpasswordEmail.js
@@ -1,53 +1,54 @@
 import React, { useState } from "react";
 import "../passwordChange/changepassword.css";
 import { changePassword } from "../../api/userRequest";
+import toast from "react-hot-toast";
 function ResetPasswordEmail() {
-  const [email, setEmail] = useState("");
+    const [email, setEmail] = useState("");
 
-  const handlePasswordChange = async (e) => {
-    e.preventDefault();
+    const handlePasswordChange = async (e) => {
+        e.preventDefault();
 
-    // if (newPassword !== confirmPassword) {
-    //   alert("New password and confirm password don't match.");
-    //   return;
-    // }
+        const { data } = await changePassword({
+            email: email,
+        });
+        console.log(data);
+        if (data.result) {
+            toast.success(data.message);
+        } else {
+            toast.error(data.message);
+        }
+    };
 
-    // You can implement your password change logic here.
-    // You may want to send an API request to your server to update the password.
+    return (
+        <div>
+        <h1 className="heading"
+          style={{
+          paddingTop:"100px"
+        }}
+        >Reset Password</h1>
+            <form className="form-making" onSubmit={handlePasswordChange}>
+                <div className="input-area-second">
+                    <label
+                        className="newpassword-label-area"
+                        htmlFor="newPassword"
+                    >
+                        Enter Your Email :
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </div>
 
-    // Reset the form after a successful password change.
-    // setCurrentPassword('');
-    // setNewPassword('');
-    // setConfirmPassword('');
-    const { data } = await changePassword({
-      email: email,
-    });
-    console.log(data);
-  };
-
-  return (
-    <div>
-      <h1 className="heading">Reset Password</h1>
-      <form className="form-making" onSubmit={handlePasswordChange}>
-        <div className="input-area-second">
-          <label className="newpassword-label-area" htmlFor="newPassword">
-            Enter Your Email :
-          </label>
-          <input
-            type="email"
-            id="email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
+                <button className="change-password-button-area" type="submit">
+                    Submit
+                </button>
+            </form>
         </div>
-
-        <button className="change-password-button-area" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default ResetPasswordEmail;
