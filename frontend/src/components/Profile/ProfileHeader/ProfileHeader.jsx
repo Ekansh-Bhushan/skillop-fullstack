175feceb88bdc3c99fbe99028de818046d8d8fc6
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-01-13 20:40:07 +0530

    edit pic

--- frontend/src/components/Profile/ProfileHeader/ProfileHeader.jsx
+++ frontend/src/components/Profile/ProfileHeader/ProfileHeader.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from "react";
-import "./ProfileHeader.css";
-import { useNavigate } from "react-router-dom";
-import { getUser } from "../../../api/userRequest";
-import userPic from "../../images/user.png";
-import defaultBGPic from "../../images/bg.png";
-import IntroVideo from "../Right Profile/IntroVideo";
-import Following from "../Right Profile/Following";
-import Followers from "../Right Profile/Followers";
-import toast from "react-hot-toast";
-import post2 from "../../images/post2.png";
+import React, { useEffect, useState } from 'react';
+import './ProfileHeader.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { getUser } from '../../../api/userRequest';
+import userPic from '../../images/user.png';
+import defaultBGPic from '../../images/bg.png';
+import IntroVideo from '../Right Profile/IntroVideo';
+import Following from '../Right Profile/Following';
+import Followers from '../Right Profile/Followers';
 
 const ProfileHeader = () => {
   const navigate = useNavigate();
@@ -28,12 +26,12 @@ const ProfileHeader = () => {
       setUserDetails(userData.data.result);
     } catch (err) {
       if (!err.response.data.result) {
-        localStorage.removeItem("skilloptoken");
-        navigate("/");
-        console.log("here is ", err.response.data.result);
+        localStorage.removeItem('skilloptoken');
+        navigate('/');
+        console.log('here is ', err.response.data.result);
         // toast.error('Session expired, Login again!');
       }
-      console.log("Unable to fetch user details", err);
+      console.log('Unable to fetch user details', err);
     }
   };
 
@@ -110,13 +108,22 @@ const ProfileHeader = () => {
                 ' @ ' + userDetails.education[0].institute}
             </p>
           )}
-          <div
-            id='ph-mypost'
-            onClick={() =>
-              userDetails && navigate(`/userposts/${userDetails._id}`)
-            }
-          >
-            My Posts
+          <div className='flex gap-3 items-center'>
+            <div
+              id='ph-mypost'
+              onClick={() =>
+                userDetails && navigate(`/userposts/${userDetails._id}`)
+              }
+            >
+              My Posts
+            </div>
+            <Link to='/editpic'>
+              <img
+                src='/editpic.png'
+                className='w-14 rounded-full p-1 cursor-pointer hover:bg-gray-100'
+                alt='edit pic'
+              />
+            </Link>
           </div>
         </div>
         <div className='ph-follow'>
@@ -152,7 +159,8 @@ const ProfileHeader = () => {
             <a
               href={
                 userDetails &&
-                (userDetails.linkedinId && userDetails.linkedinId.includes('linkedin.com')
+                (userDetails.linkedinId &&
+                userDetails.linkedinId.includes('linkedin.com')
                   ? userDetails.linkedinId
                   : `https://linkedin.com/in/${userDetails.linkedinId}`)
               }
@@ -160,7 +168,8 @@ const ProfileHeader = () => {
               rel='noreferrer'
             >
               {userDetails &&
-                (userDetails.linkedinId && userDetails.linkedinId.includes('linkedin.com')
+                (userDetails.linkedinId &&
+                userDetails.linkedinId.includes('linkedin.com')
                   ? userDetails.linkedinId
                   : `https://linkedin.com/in/${userDetails.linkedinId}`)}
             </a>
