013a62a2e6c421a593b97f7cfbf71a9729860b19
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-02-04 09:55:21 +0530

    del individual SLOTs

--- backend/src/routes/mentor.js
+++ backend/src/routes/mentor.js
@@ -1,52 +1,52 @@
-const express = require("express");
-const { isAuthorised } = require("../middleware/auth");
+const express = require('express');
+const { isAuthorised } = require('../middleware/auth');
 const {
-    becomeAMentor,
-    requestAppointment,
-    getAvailability,
-    updateAvailability,
-    deleteAvailability,
-    getMeets,
-    getActualAvailability,
-    acceptMeet,
-    getUpcommingMeet,
-    getPendingMeet,
-    getCompletedMeet,
-    getEarnings,
-    requestToBeMentor,
-    setDataForMentor,
-    createToken,
-    createMeetEvent
-} = require("../controllers/mentor");
-const { paymentConformationPicUploader } = require("../uploads/handleUploads");
+  becomeAMentor,
+  requestAppointment,
+  getAvailability,
+  updateAvailability,
+  deleteAvailability,
+  getMeets,
+  getActualAvailability,
+  acceptMeet,
+  getUpcommingMeet,
+  getPendingMeet,
+  getCompletedMeet,
+  getEarnings,
+  requestToBeMentor,
+  setDataForMentor,
+  createToken,
+  createMeetEvent,
+} = require('../controllers/mentor');
+const { paymentConformationPicUploader } = require('../uploads/handleUploads');
 const router = express.Router();
 
 // router.route("/mentor/become").put(isAuthorised, becomeAMentor);
 router
-    .route("/mentor/book/:mentorId")
-    .post(isAuthorised, paymentConformationPicUploader, requestAppointment);
+  .route('/mentor/book/:mentorId')
+  .post(isAuthorised, paymentConformationPicUploader, requestAppointment);
 router
-    .route("/mentor/availability/:mentorId")
-    .post(isAuthorised, getAvailability);
+  .route('/mentor/availability/:mentorId')
+  .post(isAuthorised, getAvailability);
 router
-    .route("/mentor/update/availability")
-    .put(isAuthorised, updateAvailability);
+  .route('/mentor/update/availability')
+  .put(isAuthorised, updateAvailability);
+router.route('/mentor/delete/slot/:slotID').delete(deleteAvailability);
+router.route('/mentor/meets').get(isAuthorised, getMeets);
+router.route('/mentor/meet/upcomming').get(isAuthorised, getUpcommingMeet);
+router.route('/mentor/meet/pending').get(isAuthorised, getPendingMeet);
+router.route('/mentor/meet/completed').get(isAuthorised, getCompletedMeet);
 router
-  .route('/mentor/delete/slot/:slotID')
-  .delete(isAuthorised, deleteAvailability);
-router.route("/mentor/meets").get(isAuthorised, getMeets);
-router.route("/mentor/meet/upcomming").get(isAuthorised, getUpcommingMeet);
-router.route("/mentor/meet/pending").get(isAuthorised, getPendingMeet);
-router.route("/mentor/meet/completed").get(isAuthorised, getCompletedMeet);
+  .route('/mentor/getAvailability')
+  .get(isAuthorised, getActualAvailability);
+router.route('/mentor/meet/accept/:meetId').put(isAuthorised, acceptMeet);
+router.route('/mentor/meet/create-tokens').post(isAuthorised, createToken);
 router
-    .route("/mentor/getAvailability")
-    .get(isAuthorised, getActualAvailability);
-router.route("/mentor/meet/accept/:meetId").put(isAuthorised, acceptMeet);
-router.route("/mentor/meet/create-tokens").post(isAuthorised, createToken);
-router.route("/mentor/meet/create-meet-event").post(isAuthorised, createMeetEvent);
+  .route('/mentor/meet/create-meet-event')
+  .post(isAuthorised, createMeetEvent);
 
-router.route("/mentor/earnings").get(isAuthorised, getEarnings);
+router.route('/mentor/earnings').get(isAuthorised, getEarnings);
 
-router.route("/mentor/request").put(isAuthorised, requestToBeMentor);
-router.route("/mentor/set").put(isAuthorised, setDataForMentor);
+router.route('/mentor/request').put(isAuthorised, requestToBeMentor);
+router.route('/mentor/set').put(isAuthorised, setDataForMentor);
 module.exports = router;
