f3f8c1721331386042e91dcd85c4a61be6eeca17
MASTERsj01 <154411672+MASTERsj01@users.noreply.github.com>
2024-02-19 17:01:56 +0530

    Update index.js
    edit account margin gap fixed

--- frontend/src/components/dashboard/account/index.js
+++ frontend/src/components/dashboard/account/index.js
@@ -87,7 +87,7 @@ function Account({ userData, setProgress}) {
 
         <div className='dash-right dash-right-2'>
           {/* <Topbar setProgress={setProgress}/> */}
-          <div className='pt-10vh h-100vh border-r-2 md:border-0 md:pt-6vh ml--35px ml--35px@media (max-width: 500px)'>
+          <div className='pt-6vh h-100vh border-r-2 md:border-0 md:pt-6vh ml--35px ml--35px@media (max-width: 500px)'>
             <h1 className='font-semibold text-2xl mb-5'>
               Edit Account Information
             </h1>
