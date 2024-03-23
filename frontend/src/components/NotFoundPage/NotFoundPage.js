02d1dd792869fe84b7ed472f5f297a08819a02e4
ashwani yadav <22ashwaniyadav@gmail.com>
2024-03-24 02:46:21 +0530

    adding usernotfound page

---
+++ frontend/src/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './NotFoundPage.css'; 
+
+// Import the background image
+import backgroundImage from './backgroungimage.png';
+
+const NotFoundPage = () => {
+  return (
+    <div className='outer'>
+      <div className='not-found-container' style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <p id='oops' className='not-found-title'>OOPS!</p>
+        <p className='not-found-title'>404</p>
+        <p className='subtitle'>PAGE NOT FOUND</p>
+        <p className='not-found-message'>The page you are looking for does not exist.</p>
+      
+        {/* Add the Link component for the button */}
+        <Link to="/" className="home-button">Go to Home</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
